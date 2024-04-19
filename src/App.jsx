0d1ec8d22385897076486beabf17b872910174fc
Rushik-03 <bodasairushik@gmail.com>
2024-04-19 16:34:37 +0530

    Project Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import {BrowserRouter as Router, Routes,Route } from 'react-router-dom'
-import Navbar from './Components/Navbar'
+import Navbar from './Navbar'
 import Home from './Home'
 import Predict from './Predict'
 
